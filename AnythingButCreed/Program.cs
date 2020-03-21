2b2ce042c91411062d422a93904ed2b8a8875334
Crystal Broach <crysland@gmail.com>
2020-03-20 21:35:08 -0500

    Add project files.

---
+++ AnythingButCreed/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AnythingButCreed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
